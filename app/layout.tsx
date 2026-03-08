2eef1f5723c4ad5c81f72105de800f667d4971ca
bimasenaadji <bimasenaadji@gmail.com>
2026-03-08 17:55:34 +0700

    feat: adding next top provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased bg-zinc-50 dark:bg-zinc-950`}
       >
+        <NextTopLoader
+          color=" #2F4F4F"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false} // Matikan spinner bundar di pojok kanan atas biar lebih clean
+          easing="ease"
+          speed={200}
+        />
         <Toaster position="top-center" reverseOrder={false} />
         {children}
         <Analytics />
