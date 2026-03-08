ebe806365e3e0f61a67e46281923ead01f7209b9
bimasenaadji <bimasenaadji@gmail.com>
2026-03-08 12:59:37 +0700

    Clean up file modules and fix page structure

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-
-const page = () => {
-  return <div></div>;
-};
-
-export default page;
+export default function PublicPage() {
+  return (
+    <main>
+      <h1>Blog Bima Sena Adji</h1>
+    </main>
+  );
+}
