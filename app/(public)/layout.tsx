1e9d7468d0bc22e2b0d43e1f69d2d63769fa7683
bimasenaadji <bimasenaadji@gmail.com>
2026-03-09 15:37:15 +0700

    feat: update PublicLayout to fetch and pass search posts to Navbar

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,15 +1,17 @@
 import Navbar from "../components/public/Navbar";
 import { Footer } from "../components/public/Footer";
+import { getSearchPosts } from "@/services/post.service";
 
-export default function PublicLayout({
+export default async function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const searchPosts = await getSearchPosts();
   return (
     <div className="flex flex-col min-h-screen bg-white">
       {/* Navbar nempel di atas terus */}
-      <Navbar />
+      <Navbar searchPosts={searchPosts} />
 
       {/* Konten utama yang bakal ganti-ganti (Hero, List Blog, dll) */}
       <main className="flex-grow">{children}</main>
